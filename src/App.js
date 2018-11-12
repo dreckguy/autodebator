de3bdd5a273fb0694d1c5b606fadd12be5a82f3d
ophir <devteam@wizer.me>
2018-11-12 12:50:39 +0200

    change to autodebator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,18 +7,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           
-        <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <img src="https://g.foolcdn.com/editorial/images/485893/debate-robot-getty.jpg" className="App-logo" alt="logo" />
+          <p>This will be The Autodebator Site!</p>
         </header>
       </div>
     );
